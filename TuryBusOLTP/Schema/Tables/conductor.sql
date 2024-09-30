8998f4156189ad47b5866205765a9b7322a06073
brandon <brandon@BRANDON96B7>
2024-09-30 16:20:34 -0400

    Add project files.

---
+++ TuryBusOLTP/Schema/Tables/conductor.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[conductor]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[DNI] VARCHAR(10) PRIMARY KEY,
+    [ApellidosNombre] NVARCHAR(100),
+    [Telefono] NVARCHAR(15),
+    [Direccion] NVARCHAR(200)
+)
